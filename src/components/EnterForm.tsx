7e510e04780a05eaf1cfca5d10d78c601e313d14
Evgenii Kolodin <ko_on@mail.ru>
2023-07-31 23:20:55 +0400

    add tailwind

---
+++ src/components/EnterForm.tsx
@@ -0,0 +1,10 @@
+
+function EnterForm() {
+  return (
+    <div className="bg-slate-400">
+      Enter form
+    </div>
+  )
+}
+
+export default EnterForm
---
+++ src/components/Lobby.tsx
@@ -0,0 +1,11 @@
+import EnterForm from "./EnterForm"
+
+function Lobby() {
+  return (
+    <div>
+      <EnterForm />
+    </div>
+  )
+}
+
+export default Lobby
